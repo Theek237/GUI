4eccb2fec54976bad4a429ca01f4e03a7de1730e
Theekshana Alahakoon <theek237@gmail.com>
2024-12-28 17:08:06 +0530

    sign in page created

--- src/Pages/Sign In/SignIn.jsx
+++ src/Pages/Sign In/SignIn.jsx
@@ -1,12 +1,53 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
-export default function SignIn() {
+import { Link } from "react-router-dom";
+
+import "./signin.css";
+export default function SignIn(props) {
   return (
     <>
       <>
         <Header />
-        <div>SignIn</div>
-        <div>Page</div>
+        <div className="signin-container">
+          <div className="signin-leftside">
+            <img src="src\assets\login.png" alt="loginimg" />
+          </div>
+          <div className="signin-rightside">
+            <div className="signin-title">
+              <h1>
+                <span>Student</span> Sign In
+              </h1>
+              <h2>Enter Your Email and Password</h2>
+            </div>
+            <div className="signin-input">
+              <div className="input-email">
+                <input type="email" placeholder="Email" className=" inputbox" />
+                <img src="src/assets/email.png" alt="" className="input-icon" />
+              </div>
+
+              <div className="input-password">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className=" inputbox"
+                />
+                <img
+                  src="src/assets/password.png"
+                  alt=""
+                  className="input-icon"
+                />
+              </div>
+            </div>
+
+            <button className="signin-btn">Sign In</button>
+            <p className="linktosignup">
+              Don’t Have an Account?{" "}
+              <span>
+                <Link to="/signup">Sign Up</Link>
+              </span>
+            </p>
+          </div>
+        </div>
       </>
     </>
   );
