7a6dcf9a20b65af57a0937fb67a937aa05fe1882
Theekshana Alahakoon <theek237@gmail.com>
2025-01-18 02:31:04 +0530

    started to working on desktop app

---
+++ Desktop App/Eduverse Admin Desktop Application/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eduverse_Admin_Desktop_Application.Models
+{
+    internal class Student
+    {
+    }
+}
